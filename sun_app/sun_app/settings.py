c531a08e95e88f410700b25b7b03608946b8412a
Comimit inicial <Jeanmob_2503@hotmail.com>
2023-06-30 12:04:36 -0500

    Commit inicial

--- sun_app/sun_app/settings.py
+++ sun_app/sun_app/settings.py
@@ -76,12 +76,21 @@ WSGI_APPLICATION = 'sun_app.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
+   }
 }
-
+#DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#       'NAME': 'ueyudxqp',
+#       'USER': 'ueyudxqp',
+#       'PASSWORD': 'MkQkHSGwER6LRhyed6hUCUkflyB2Elg3',
+#       'HOST': 'stampy.db.elephantsql.com',
+#       'PORT': '5432',
+#   }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
